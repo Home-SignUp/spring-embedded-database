6db960b94165b3c46dc005db962744ac27949934
Sasha <skmets@gmail.com>
2017-04-27 14:13:01 +0300

    Spring+Database_java

--- src/main/java/com/mkyong/dao/UserDaoImpl.java
+++ src/main/java/com/mkyong/dao/UserDaoImpl.java
@@ -35,19 +35,17 @@ public class UserDaoImpl implements UserDao {
 	public User findByName(String name) {
 		Map<String, Object> params = new HashMap<String, Object>();
         params.put("name", name);
-		String sql = "SELECT * FROM users WHERE name=:name";
-		
-        User result = namedParameterJdbcTemplate.queryForObject(sql, params, new UserMapper());
+        User result = namedParameterJdbcTemplate.queryForObject(FIND_BY_NAME_QUERY, params, new UserMapper());
         //new BeanPropertyRowMapper(Customer.class));
+
         return result;
 	}
 
 	@Override
 	public List<User> findAll() {
 		Map<String, Object> params = new HashMap<String, Object>();
-		String sql = "SELECT * FROM users";
+        List<User> result = namedParameterJdbcTemplate.query(FIND_ALL_QUERY, params, new UserMapper());
 
-        List<User> result = namedParameterJdbcTemplate.query(sql, params, new UserMapper());
         return result;
 	}
 
@@ -58,6 +56,7 @@ public class UserDaoImpl implements UserDao {
 			user.setId(rs.getInt("id"));
 			user.setName(rs.getString("name"));
 			user.setEmail(rs.getString("email"));
+
 			return user;
 		}
 	}
